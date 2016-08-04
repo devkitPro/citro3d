59b0311f4844a38ce15326c62aeae5c0e9c2834b
Thompson Lee <tom.mai78101@gmail.com>
2016-08-04 07:05:25 -0400

    Minor Fixes series - Added a missing @param[in]. - Added @mtheall 's Mtx_Multiply() enhancement.

--- source/maths/mtx_multiply.c
+++ source/maths/mtx_multiply.c
@@ -2,6 +2,15 @@
 
 void Mtx_Multiply(C3D_Mtx* out, const C3D_Mtx* a, const C3D_Mtx* b)
 {
+	// if out is a or b, then we need to avoid overwriting them
+	if(out == a || out == b)
+	{
+		C3D_Mtx tmp;
+		Mtx_Multiply(&tmp, a, b);
+		Mtx_Copy(out, &tmp);
+		return;
+	}
+
 	// http://www.wolframalpha.com/input/?i={{a,b,c,d},{e,f,g,h},{i,j,k,l},{m,n,o,p}}{{α,β,γ,δ},{ε,θ,ι,κ},{λ,μ,ν,ξ},{ο,π,ρ,σ}}
 	int i, j;
 	for (j = 0; j < 4; ++j)
