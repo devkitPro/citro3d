141afdb18a81a76c675b204022824ab44b7d0e81
fincs <fincs.alt1@gmail.com>
2017-02-14 19:40:59 +0100

    Correct inaccurate WolframAlpha link in mtx_persp.c

--- source/maths/mtx_persp.c
+++ source/maths/mtx_persp.c
@@ -7,7 +7,7 @@ void Mtx_Persp(C3D_Mtx* mtx, float fovy, float aspect, float near, float far, bo
 	Mtx_Zeros(mtx);
 
 	// Standard perspective projection matrix, with a fixed depth range of [-1,0] (required by PICA)
-	// http://www.wolframalpha.com/input/?i={{1,0,0,0},{0,1,0,0},{0,0,0.5,-0.5},{0,0,0,1}}{{1/(a*tan(v)),0,0,0},{0,1/tan(v),0,0},{0,0,(n%2Bf)/(n-f),(fn)/(n-f)},{0,0,0,-1}}
+	// http://www.wolframalpha.com/input/?i={{1,0,0,0},{0,1,0,0},{0,0,0.5,-0.5},{0,0,0,1}}{{1/(a*tan(v)),0,0,0},{0,1/tan(v),0,0},{0,0,(2%2Bn%2Bf)/(n-f),(fn)/(n-f)},{0,0,-1,0}}
 
 	mtx->r[0].x = 1.0f / (aspect * fovy_tan);
 	mtx->r[1].y = 1.0f / fovy_tan;
