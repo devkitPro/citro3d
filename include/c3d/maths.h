401906673d1e0a7406a5e99352d52fb08bfb07df
Teodor Spæren <teodor@sparen.no>
2022-05-28 14:37:03 +0200

    Add M_PI to maths, to allow compilation on C++
    C++ does not specify `M_PI`, so this change is required to allow for compilation.

--- include/c3d/maths.h
+++ include/c3d/maths.h
@@ -9,6 +9,10 @@
  * @{
  */
 
+#ifndef M_PI
+#    define M_PI 3.14159265358979323846264338327950288
+#endif
+
 /**
  * The one true circumference-to-radius ratio.
  * See http://tauday.com/tau-manifesto
